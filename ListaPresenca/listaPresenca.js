ea3236570ca27581e7d31e474be2fbccfde9fb86
Don_Aveo <133364365+BrendonAvelino@users.noreply.github.com>
2024-09-26 09:45:13 -0300

    update listaPresenca.js

--- ListaPresenca/listaPresenca.js
+++ ListaPresenca/listaPresenca.js
@@ -1,14 +1,22 @@
 // input container
 function toggleDropdown() {
+  const dropdown = document.getElementById("dropdown");
+  dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches('.custom-input')) {
     const dropdown = document.getElementById("dropdown");
-    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
-  }
-  
-  window.onclick = function(event) {
-    if (!event.target.matches('.custom-input')) {
-      const dropdown = document.getElementById("dropdown");
-      if (dropdown.style.display === "block") {
-        dropdown.style.display = "none";
-      }
+    if (dropdown.style.display === "block") {
+      dropdown.style.display = "none";
     }
   }
+}
+
+function adjustTableContainerHeight() {
+  dropdown.style.maxHeight = '100px';
+  dropdown.style.overflowY = 'auto';
+}
+
+adjustTableContainerHeight();
+window.addEventListener('resize', adjustTableContainerHeight);
