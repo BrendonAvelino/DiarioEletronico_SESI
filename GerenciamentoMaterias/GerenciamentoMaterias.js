04c6055fd598dbfcba979a6fb2598fa9801a00f3
Don_Aveo <133364365+BrendonAvelino@users.noreply.github.com>
2024-10-03 15:18:52 -0300

    update responsivo gerenciarTarefas

--- GerenciamentoMaterias/GerenciamentoMaterias.js
+++ GerenciamentoMaterias/GerenciamentoMaterias.js
@@ -34,7 +34,7 @@ window.addEventListener('load', () => {
         rows.forEach(row => {
             const subjectCell = row.cells[3];
             const statusIndicator = row.querySelector('.status-indicator');
-            
+
             if (subjectCell && statusIndicator) {
                 const subject = subjectCell.textContent.trim();
                 const color = subjectColors[subject] || '#FF0000';
@@ -75,4 +75,18 @@ window.addEventListener('load', () => {
             closeModal(modal2);
         }
     });
+
+    // TextData
+    const dataAtual = new Date();
+
+    // Extrai o dia, mês e ano
+    const dia = String(dataAtual.getDate()).padStart(2, '0'); // Adiciona '0' se for necessário
+    const mes = String(dataAtual.getMonth() + 1).padStart(2, '0'); // Adiciona 1 ao mês (começa em 0)
+    const ano = dataAtual.getFullYear();
+
+    // Formata a data como DD/MM/YYYY
+    const dataFormatada = `${dia}/${mes}/${ano}`;
+
+    // Exibe a data no elemento com id "TextData"
+    document.getElementById('TextData').textContent = dataFormatada;
 });
